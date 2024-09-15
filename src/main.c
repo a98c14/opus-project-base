8b2cdf1c1cd51c00a81c145a83749988e792280d
a98c14 <selimyesilkaya@gmail.com>
2024-09-15 20:48:48 +0300

    update

--- src/main.c
+++ src/main.c
@@ -3,18 +3,28 @@
 #include "opus/base/base_inc.h"
 #include "opus/base/base_inc.c"
 
-typedef struct
-{
-    int32 x;
-} TestStruct;
+#include "opus/os/os_inc.h"
+#include "opus/os/os_inc.c"
 
-int
+int32
 main()
 {
-    Arena* persistent_arena = arena_new();
+    ThreadContext tctx;
+    tctx_init_and_equip(&tctx);
 
-    TestStruct* test_struct = arena_push_struct(persistent_arena, TestStruct);
-    test_struct->x          = 5;
-    int32 t                 = 12;
-    printf("test %d", test_struct->x);
+    Arena* persistent_arena = arena_new();
+    String file_path        = string("C:\\.ignore\\test.txt");
+    if (os_file_exists_at_path(file_path))
+    {
+        OS_Handle file     = os_file_open(file_path, OS_AccessFlag_Read);
+        String    contents = os_file_read_all_as_string(file, persistent_arena);
+        log_info("file contents: %s", contents.value);
+
+        String working_directory = os_file_get_working_directory(persistent_arena);
+        printf("working_directory: %s\n", working_directory.value);
+    }
+    else
+    {
+        log_info("file doesnt exist");
+    }
 }
